1553cba002a31814eaed7afec760f072be5f31b8
Andrew Locke <alocke12992@gmail.com>
2018-11-27 19:19:36 -0800

    builds form

--- countDown/src/App.js
+++ countDown/src/App.js
@@ -3,12 +3,48 @@ import React, { Component } from 'react'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      hour: null,
+      min: null,
+      sec: null
+    }
+  }
+
+  handleChange(e){
+    e.preventDefault()
+
+  } 
+
+  handleSubmit(e){
+    e.preventDefault()
   }
 
   render() {
     return (
       <div className="App">
+        <form onSubmit={() => this.handleSubmit()}>
+          <input 
+            name="hour"
+            placeholder="Hours"
+            type="number"
+            value={this.state.hour}
+            onChange={() => this.handleChange}
+          />
+          <input 
+            name="min"
+            placeholder="Minutes"
+            type="number"
+            value={this.state.min}
+            onChange={() => this.handleChange}
+          />
+          <input 
+            name="sec"
+            placeholder="Seconds"
+            type="number"
+            value={this.state.sec}
+            onChange={() => this.handleChange}
+          />
+        </form>
       </div>
     )
   }
