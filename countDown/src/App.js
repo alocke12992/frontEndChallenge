20c58c8966ab04a4e766cc65f9b815256a5cc901
Andrew Locke <alocke12992@gmail.com>
2018-11-27 20:02:03 -0800

    Adds counter

--- countDown/src/App.js
+++ countDown/src/App.js
@@ -1,50 +1,88 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import './App.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      hour: null,
-      min: null,
-      sec: null
+      sec: 0,
+      counting: false
     }
   }
 
   handleChange(e){
-    e.preventDefault()
-
+    this.setState({sec: time})
   } 
 
   handleSubmit(e){
     e.preventDefault()
+    let time = this.refs.sec.value
+    this.setState({
+      sec
+      counting: true
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState, newProps, newState){
+    console.log(prevProps, prevState, newState, newProps)
+    if (this.state.counting){
+      this.handleCountDown()
+    }
+  }
+
+  handleCountDown() {
+    setInterval(() => {
+      let timeRemaining = this.state.sec - 1
+      this.setState({ 
+        sec: timeRemaining >= 0 ? timeRemaining : 0 
+      })
+    }, 1000)
+  }
+
+  showForm(){
+    return(
+      <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input 
+            name="sec"
+            placeholder="Seconds"
+            type="number"
+            ref="sec"
+          />
+          <button type="submit">
+            Start
+          </button>
+        </form>
+    )
+  }
+
+  formatTime(time){
+    let seconds = time % 60;
+    let minutes = Math.floor(time / 60);
+    minutes = minutes.toString().length === 1 ? "0" + minutes : minutes;
+    seconds = seconds.toString().length === 1 ? "0" + seconds : seconds;
+    return minutes + ':' + seconds;
+  }
+
+  ticker(){
+    return(
+
+    <div>
+      {this.formatTime(this.state.sec)}
+    </div>
+    )
   }
 
   render() {
     return (
       <div className="App">
-        <form onSubmit={() => this.handleSubmit()}>
-          <input 
-            name="hour"
-            placeholder="Hours"
-            type="number"
-            value={this.state.hour}
-            onChange={() => this.handleChange}
-          />
-          <input 
-            name="min"
-            placeholder="Minutes"
-            type="number"
-            value={this.state.min}
-            onChange={() => this.handleChange}
-          />
-          <input 
-            name="sec"
-            placeholder="Seconds"
-            type="number"
-            value={this.state.sec}
-            onChange={() => this.handleChange}
-          />
-        </form>
+        <Fragment>
+          {this.showForm()}
+        </Fragment>
+        <Fragment>
+          {
+            this.ticker()
+          }
+        </Fragment>
       </div>
     )
   }
