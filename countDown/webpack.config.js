47e38268bb9b3d0dcf99041ae7ce09a29554ea62
Andrew Locke <alocke12992@gmail.com>
2018-11-27 19:06:42 -0800

    countdown frame

---
+++ countDown/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        loaders: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".css"]
+  }
+};
